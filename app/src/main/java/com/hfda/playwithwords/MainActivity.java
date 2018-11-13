439c7f4f6c128d48d74cbc3f3bf84d58e2a23235
ThanhHuong98 <ttinhoc11@gmail.com>
2018-11-13 07:00:47 +0700

    Style
    update Style for tool bar

--- app/src/main/java/com/hfda/playwithwords/MainActivity.java
+++ app/src/main/java/com/hfda/playwithwords/MainActivity.java
@@ -4,12 +4,15 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
@@ -28,6 +31,7 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/*----------------------*/
 
         //đòi quyển truy cập
 
