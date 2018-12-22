61e00f4193cd531bf41fd41b8424facf78e8b380
toandaominh1997 <toandaominh1997@gmail.com>
2018-12-22 21:32:55 +0700

    Create CameraActivity

--- app/src/main/java/org/tensorflow/demo/CameraActivity.java
+++ app/src/main/java/org/tensorflow/demo/CameraActivity.java
@@ -1,4 +1,57 @@
 package org.tensorflow.demo;
 
-public class CameraActivity {
+import android.Manifest;
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.Image.Plane;
+import android.media.ImageReader;
+import android.media.ImageReader.OnImageAvailableListener;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Trace;
+import android.util.Size;
+import android.view.KeyEvent;
+import android.view.Surface;
+import android.view.WindowManager;
+import android.widget.Toast;
+import java.nio.ByteBuffer;
+import org.tensorflow.demo.env.ImageUtils;
+import org.tensorflow.demo.env.Logger;
+import com.hfda.playwithwords.R;
+
+public abstract class CameraActivity extends Activity
+        implements OnImageAvailableListener, Camera.PreviewCallback {
+    private static final Logger LOGGER = new Logger();
+
+    private static final int PERMISSIONS_REQUEST = 1;
+
+    private static final String PERMISSION_CAMERA = Manifest.permission.CAMERA;
+    private static final String PERMISSION_STORAGE = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+    private boolean debug = false;
+
+    private Handler handler;
+    private HandlerThread handlerThread;
+    private boolean useCamera2API;
+    private boolean isProcessingFrame = false;
+    private byte[][] yuvBytes = new byte[3][];
+    private int[] rgbBytes = null;
+    private int yRowStride;
+
+    protected int previewWidth = 0;
+    protected int previewHeight = 0;
+
+    private Runnable postInferenceCallback;
+    private Runnable imageConverter;
+    
 }
